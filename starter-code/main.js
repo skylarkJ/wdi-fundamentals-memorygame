600ed8809cca039feedd63dfd2b637df4062d899
Jana Ryndin <jana.ryndin@gmail.com>
2016-12-29 17:16:47 -0500

    adding comparing values to js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,13 @@
 console.log("JS file is connected to HTML! Woo!")
 
+// Created 4 cards with 4 string values
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+//comparing two queen variables to check for equality = true
+cardOne == cardTwo
+
+// comparing a queen variable with a king variable = false
+cardOne == cardFour
